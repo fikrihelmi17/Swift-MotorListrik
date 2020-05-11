0655b5f48af35a5015c46eb3b2a72234a21b9739
fikri <fikrihelmi17@gmail.com>
2020-05-11 08:58:30 +0700

    Initial Commit

---
+++ MotorListrik/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MotorListrik
+//
+//  Created by Fikri on 11/05/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
